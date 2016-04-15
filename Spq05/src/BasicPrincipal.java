e6cf9b26a8ca7d5b2073249ccc10a6d71593bcbc
jon bilbao <astrainjb25@gmail.com>
2016-04-15 15:00:38 +0200

    v0.004

---
+++ Spq05/src/BasicPrincipal.java
@@ -0,0 +1,147 @@
+import javax.swing.JFrame;
+import java.awt.Panel;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.awt.BorderLayout;
+import java.awt.GridBagLayout;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import java.awt.GridBagConstraints;
+import javax.swing.JLabel;
+import java.awt.Insets;
+import javax.swing.JComboBox;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
+public class BasicPrincipal extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private JTable table;
+	protected String[] names = {"Telepizza", "Deustoarrak ", "Foster" };
+	protected String[] places = {"Bilbao", "Gorliz ", "Getxo" };
+	protected String[] categorys = {"Mexican", "Italian ", "American", "Asian" };
+	protected String[] rates = {"1", "2", "3", "4", "5" };
+
+	protected String[] namesTriki = { "Name","Telepizza", "Deustoarrak ", "Foster" };
+	protected String[] placesTriki = { "Place","Bilbao", "Gorliz ", "Getxo" };
+	protected String[] categorysTriki = { "Category","Mexican", "Italian ", "American", "Asian" };
+	protected String[] ratesTriki = { "Rate","1", "2", "3", "4", "5" };
+
+	public BasicPrincipal() {
+		
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(700, 600);
+		Panel panel = new Panel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+
+		JLabel UserNameLabel = new JLabel("User A");
+		UserNameLabel.setBounds(469, 34, 54, 14);
+		UserNameLabel.setEnabled(false);
+		panel.add(UserNameLabel);
+
+		JButton LogoutButton = new JButton("Logout");
+		LogoutButton.setBounds(542, 30, 81, 23);
+		panel.add(LogoutButton);
+
+		JComboBox<String> comboBoxName = new JComboBox<String>();
+		comboBoxName.setBounds(93, 88, 120, 20);
+		comboBoxName.setModel(new DefaultComboBoxModel<String>(namesTriki));
+		panel.add(comboBoxName);
+		comboBoxName.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				comboBoxName.setModel(new DefaultComboBoxModel<String>(namesTriki));
+				comboBoxName.setSelectedIndex(0);
+				repaint();
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				comboBoxName.setModel(new DefaultComboBoxModel<String>(names));
+				repaint();
+			}
+		});
+
+		JComboBox<String> comboBoxCategory = new JComboBox<String>();
+		comboBoxCategory.setBounds(278, 88, 96, 20);
+		comboBoxCategory.setModel(new DefaultComboBoxModel<String>(categorysTriki));
+		panel.add(comboBoxCategory);
+		comboBoxCategory.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				comboBoxCategory.setModel(new DefaultComboBoxModel<String>(categorysTriki));
+				comboBoxCategory.setSelectedIndex(0);
+				repaint();
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				comboBoxCategory.setModel(new DefaultComboBoxModel<String>(categorys));
+				repaint();
+			}
+		});
+
+		JComboBox<String> comboBoxPlace = new JComboBox<String>();
+		comboBoxPlace.setBounds(93, 113, 120, 20);
+		comboBoxPlace.setModel(new DefaultComboBoxModel<String>(placesTriki));
+		panel.add(comboBoxPlace);
+		comboBoxPlace.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				comboBoxPlace.setModel(new DefaultComboBoxModel<String>(placesTriki));
+				comboBoxPlace.setSelectedIndex(0);
+				repaint();
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				comboBoxPlace.setModel(new DefaultComboBoxModel<String>(places));
+				repaint();
+			}
+		});
+
+		JComboBox<String> comboBoxRate = new JComboBox<String>();
+		comboBoxRate.setBounds(278, 113, 96, 20);
+		comboBoxRate.setModel(new DefaultComboBoxModel<String>(ratesTriki));
+		panel.add(comboBoxRate);
+		comboBoxRate.addFocusListener(new FocusListener() {
+
+			@Override
+			public void focusLost(FocusEvent e) {
+				comboBoxRate.setModel(new DefaultComboBoxModel<String>(ratesTriki));
+				comboBoxRate.setSelectedIndex(0);
+				repaint();
+			}
+
+			@Override
+			public void focusGained(FocusEvent e) {
+				comboBoxRate.setModel(new DefaultComboBoxModel<String>(rates));
+				repaint();
+			}
+		});
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(60, 168, 477, 303);
+		panel.add(scrollPane);
+
+		table = new JTable();
+		scrollPane.setViewportView(table);
+
+		JButton btnFind = new JButton("Find");
+		btnFind.setBounds(434, 97, 89, 23);
+		panel.add(btnFind);
+		
+		setVisible(true);
+	}
+	public static void main(String[] args) {
+		new BasicPrincipal();
+	}
+}
