0eb50f2dad13fa8ef7ff3b07914fb58c4c29ff55
gorka <gorka@GOrka.GOrka>
2016-05-14 20:53:01 +0200

    test3

--- src/test/java/es/deusto/server/RestaurantAdvisorTest.java
+++ src/test/java/es/deusto/server/RestaurantAdvisorTest.java
@@ -573,8 +573,8 @@ public class RestaurantAdvisorTest {
 		new Basic_RestaurantWindow();
 //		new Delegate_LoginWindow("127.0.0.1", "1099", "RestaurantAdvisor");
 		new Delegate_RestaurantWindow(r,"127.0.0.1", "1099", "RestaurantAdvisor");
-//		Delegate_RegistrationWindow dr = new Delegate_RegistrationWindow("127.0.0.1", "1099", "RestaurantAdvisor");
-		new Delegate_MainWindow("127.0.0.1", "1099", "RestaurantAdvisor");
+		Delegate_RegistrationWindow dr = new Delegate_RegistrationWindow("127.0.0.1", "1099", "RestaurantAdvisor");
+//		new Delegate_MainWindow("127.0.0.1", "1099", "RestaurantAdvisor");
 		dr.getData();
 	}
 
