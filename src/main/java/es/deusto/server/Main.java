08a989323ddf75ac7be5c94f5319b3bbd4bd811a
gorka <gorka@GOrka.GOrka>
2016-05-17 13:23:30 +0200

    funciona ya toda la parte de reservas/premium

--- src/main/java/es/deusto/server/Main.java
+++ src/main/java/es/deusto/server/Main.java
@@ -14,7 +14,7 @@ public class Main {
 			
 			IServer server = new Server();
 //			((Server) server).createDatabase();
-
+//
 			Naming.rebind(serverName, server);
 			System.out.println(" Server "+serverName+" active and waiting...");
 			java.io.InputStreamReader inputStreamReader = new java.io.InputStreamReader ( System.in );
